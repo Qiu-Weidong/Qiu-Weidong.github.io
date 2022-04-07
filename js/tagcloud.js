00bc076cb9c35bf60843028704d8fbdd8299e8db
Qiu-Weidong <568385476@qq.com>
2022-04-07 23:01:23 +0800

    Site updated: 2022-04-07 23:01:22

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -18,8 +18,8 @@
          TagCanvas.textColour = '#333';
          TagCanvas.textHeight = 25;
          TagCanvas.outlineColour = '#E2E1D1';
-         TagCanvas.maxSpeed = 0.5;
-         TagCanvas.freezeActive = false;
+         TagCanvas.maxSpeed = 0.4;
+         TagCanvas.freezeActive = true;
          TagCanvas.outlineMethod = 'block';
          TagCanvas.minBrightness = 0.2;
          TagCanvas.depth = 0.92;
